e3f631f6239bb499538732c97d931739c3ba0154
Matt Beland <matt@smoothsailingsolutions.com>
2020-06-21 09:32:45 -0700

    Adds minimal setup.py and MANIFEST.in files

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+import setuptools
+
+
+setuptools.setup(
+    name="Project Name",
+    version="0.0.1",
+    author="Matt Beland",
+    author_email="matt@rearviewmirror.org",
+    description="Stuff that does things",
+    packages=setuptools.find_packages(),
+)
